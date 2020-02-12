4cbe35250a276a71f060d88f06c2e5a103a61bd4
Nem Ekpunobi <nemx14@gmail.com>
2020-02-12 00:49:29 -0500

    update dockerfile; enable pdf print

--- Extensions/AppMode.cs
+++ Extensions/AppMode.cs
@@ -1,17 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Certitrack.Extensions
+﻿namespace Certitrack.Extensions
 {
     public static class AppMode
     {
-    #if DEBUG
+#if DEBUG
         public const bool isDebug = true;
-    #else
+#else
         public const bool isDebug = false;
-    #endif
+#endif
     }
 }
